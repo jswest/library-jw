2a8fc7ac645641c4dc2ab5b2ec74205478d1c9cc
John West <john.sheffield.west@gmail.com>
2013-10-08 23:40:06 -0500

    0.1.1 Adds standalone scripts for funzies.

---
+++ scripts/completed.js
@@ -0,0 +1,33 @@
+var MongoClient = require( 'mongodb' ).MongoClient
+,		ObjectID = require( 'mongodb' ).ObjectID;
+
+MongoClient.connect(
+	"mongodb://localhost/library-jw",
+	function( error, db ) {
+		if ( error ) {
+			console.log( error );
+		} else {
+			console.log( 'Spooling up Spaulding Avenue Library...' );
+
+			var books = db.collection( 'books' );
+			books.update(
+				{},
+				{ $set: 
+					{
+						completed: 0
+					}
+				},
+				{ multi: true },
+				function ( error, numberOfChangedObjects ) {
+					if ( error ) {
+						console.log( error );
+					}
+					else {
+						console.log( 'number of changed objects: ' + numberOfChangedObjects );
+					}
+				}
+			);
+
+		}
+	}
+);
